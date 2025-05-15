e57138587762caa82d82f25ca0279a6c9d9b1d6f
Wonderboi693 <nlmhuy0101@gmail.com>
2025-05-15 15:56:48 +0700

    Add files via upload

---
+++ dcmm/dcmm/Program.cs
@@ -0,0 +1,26 @@
+﻿using SplashKitSDK;
+
+namespace CreatingUserInterfaces
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // open a window and clear it to white
+            Window window = new Window("My Interface!", 800, 600);
+            //while (!window.CloseRequested)
+            {
+                SplashKit.ProcessEvents();
+                window.Clear(Color.White);
+
+                SplashKit.DrawText("Nguyen Lam Minh Huy. ID: 105543704", Color.Black, 50, 50);
+
+                window.Refresh();
+                SplashKit.Delay(704);
+            }
+
+            // close all open windows
+            SplashKit.CloseAllWindows();
+        }
+    }
+}
---
+++ nunits/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace QueueApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nguyen Lam Minh Huy. ID: 105543704.");
+        }
+    }
+}
